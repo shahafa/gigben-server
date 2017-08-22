b2c035f3723e04be9078c783f6df5ecea77a0dbb
Shahaf Shaked <shahafs@welldone-software.com>
2017-08-22 08:01:42 +0300

    Send notification email on new user

--- controllers/earlyAccessUser.js
+++ controllers/earlyAccessUser.js
@@ -1,3 +1,4 @@
+const nodemailer = require('nodemailer');
 const uuid = require('uuid/v4');
 const EarlyAccess = require('../models/EarlyAccess');
 const { successObject, errorObject } = require('../lib/util');
@@ -6,6 +7,27 @@ const {
   ERROR_SOMETHING_BAD_HAPPEND,
   ERROR_EMAIL_ALREADY_EXISTS,
 } = require('../consts');
+
+const sendNewEarlyAccessUserNotificationEmail = (earlyAccessUserEmail) => {
+  const transporter = nodemailer.createTransport({
+    host: process.env.SMTP_HOST,
+    port: process.env.SMTP_PORT,
+    secure: true,
+    auth: {
+      user: process.env.SMTP_USER,
+      pass: process.env.SMTP_PASS,
+    },
+  });
+
+  const mailOptions = {
+    from: '"Gigben" <do-not-reply@gigben.com>',
+    to: 'shahaf@gigben.com; moshe@gigben.com; ofer@gigben.com',
+    subject: '👻 New early access user signed up!',
+    text: `New user ${earlyAccessUserEmail} signed up`,
+  };
+
+  transporter.sendMail(mailOptions);
+};
 
 const addUser = async (req, res) => {
   req.assert('email', 'email field is missing').notEmpty();
@@ -29,7 +51,7 @@ const addUser = async (req, res) => {
 
     await earlyAccessUser.save();
 
-    // sendVerificationEmail(user.email, verificationCode);
+    sendNewEarlyAccessUserNotificationEmail(req.body.email);
 
     return res.send(successObject('Early access sign up success'));
   } catch (err) {
